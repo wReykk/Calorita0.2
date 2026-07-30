020ef3633fbed13d5d6d9008cfcc70a49b428336
drew <175341438+wReykk@users.noreply.github.com>
2026-07-30 08:59:14 +0200

    feat: add user parameters update endpoint, calorie calculation logic, and fix Prisma schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,8 @@ import productRoutes from './routes/product.routes.js';
 import authRoutes from './routes/auth.routes.js';
 import { errorHandler } from './middlewares/error.middleware.js';
 import diaryRouter from './routes/diary.routes.js';
+import userRoutes from './routes/user.routes.js'
+import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,6 +16,10 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/diary', diaryRouter);
+
+// app.get('/ping', (req, res) => res.send('pong'))
+
+app.use('/api/users', userRoutes)
 
 app.use(errorHandler)
 
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -3,6 +3,8 @@ import * as userController from '../controllers/user.controller.js';
 
 const router = express.Router();
 
+// router.get('/test', (req, res) => res.send('router is working!'));
+
 router.patch('/:id/parameters', userController.updateParameters);
 
 export default router;
