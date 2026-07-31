bf61ec8bf448e1b99312f81f4895f436db61bfdc
drew <175341438+wReykk@users.noreply.github.com>
2026-07-31 17:20:20 +0200

    feat: add target weight and pace to DB, update macros calculator

--- backend/src/utils/calculator.ts
+++ backend/src/utils/calculator.ts
@@ -1,4 +1,4 @@
-import { Sex, ActivityLevel, Goal, type User } from "@prisma/client"
+import { Sex, ActivityLevel, Goal, Pace } from "@prisma/client"
 
 interface UserParams {
     weight: number,
@@ -6,11 +6,13 @@ interface UserParams {
     age: number,
     sex: Sex,
     activityLevel: ActivityLevel,
-    goal: Goal
+    goal: Goal,
+    pace: Pace,
+    targetWeight?: number
 }
 
 export function calculateMacros(params: UserParams) {
-    const { weight, height, age, sex, activityLevel, goal } = params
+    const { weight, height, age, sex, activityLevel, goal, pace, targetWeight } = params
 
     let bmr = (10 * weight) + (6.25 * height) - (5 * age);
     bmr = sex === 'MALE' ? bmr + 5 : bmr - 161;
@@ -24,12 +26,25 @@ export function calculateMacros(params: UserParams) {
     }
 
     let tdee = bmr * activityMultipliers[activityLevel];
-
     let dailyCalories = tdee;
+
     if (goal === 'LOSE') {
-        dailyCalories *= 0.8;
+        const loseMultipliers: Record<Pace, number> = {
+            EASY: 0.9,
+            MEDIUM: 0.8,
+            HARD: 0.7,
+        };
+        dailyCalories *= loseMultipliers[pace];
+
+        if (dailyCalories < 1200) dailyCalories = 1200;
+
     } else if (goal === 'GAIN') {
-        dailyCalories *= 1.15;
+        const gainMultipliers: Record<Pace, number> = {
+            EASY: 1.075,
+            MEDIUM: 1.15,
+            HARD: 1.25,
+        };
+        dailyCalories *= gainMultipliers[pace];
     }
 
     dailyCalories = Math.round(dailyCalories);
@@ -38,10 +53,29 @@ export function calculateMacros(params: UserParams) {
     const dailyFat = Math.round((dailyCalories * 0.3) / 9);
     const dailyCarbs = Math.round((dailyCalories * 0.4) / 4);
 
+    let estimatedWeeksToGoal: number | null = null
+
+    if (targetWeight && targetWeight !== weight && goal !== 'MAINTAIN') {
+        const dailyDiff = Math.abs(tdee - dailyCalories)
+
+        if (dailyDiff > 0) {
+            const weightDiff = Math.abs(weight - targetWeight);
+
+            const totalCaloriesDiff = weightDiff * 7700;
+
+            const totalDays = totalCaloriesDiff / dailyDiff;
+
+            estimatedWeeksToGoal = Math.ceil(totalDays / 7);
+        }
+    }
+
+
+
     return {
         dailyCalories,
         dailyProtein,
         dailyFat,
-        dailyCarbs
+        dailyCarbs,
+        estimatedWeeksToGoal
     };
 }
